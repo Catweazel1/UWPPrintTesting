74b3ebb709f82065ad93ad2c2c5888aaeb5eecf3
Lonnie Linde <lonnie_linde@hotmail.com>
2019-02-12 15:13:18 +0100

    Code behind added to MainPage

--- UWPPrintTesting/MainPage.xaml.cs
+++ UWPPrintTesting/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Graphics.Printing;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,14 +23,51 @@ namespace UWPPrintTesting
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private PrintHelper printHelper;
+
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        private void OnPrintButtonClick(object sender, RoutedEventArgs e)
+        private async void OnPrintButtonClick(object sender, RoutedEventArgs e)
         {
+            await printHelper.ShowPrintUIAsync();
+        }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (PrintManager.IsSupported())
+            {
+                // Tell the user how to print
+                MainPage.Current.NotifyUser("Print contract registered with customization, use the Print button to print.", NotifyType.StatusMessage);
+            }
+            else
+            {
+                // Remove the print button
+                InvokePrintingButton.Visibility = Visibility.Collapsed;
+
+                // Inform user that Printing is not supported
+                MainPage.Current.NotifyUser("Printing is not supported.", NotifyType.ErrorMessage);
+
+                // Printing-related event handlers will never be called if printing
+                // is not supported, but it's okay to register for them anyway.
+            }
+
+            // Initialize common helper class and register for printing
+            printHelper = new PrintHelper(this);
+            printHelper.RegisterForPrinting();
+
+            // Initialize print content for this scenario
+            printHelper.PreparePrintContent(new PageToPrint());
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            if(printHelper != null)
+            {
+                printHelper.UnregisterForPrinting();
+            }
         }
     }
 }
