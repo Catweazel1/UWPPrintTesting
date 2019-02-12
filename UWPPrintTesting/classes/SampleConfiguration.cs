dd5e8ef7db17670ae11da94e8e8e729318192aad
TacocaTNL <garslade359@gmail.com>
2019-02-12 22:44:31 +0100

    Started SampleConfiguration.cs

---
+++ UWPPrintTesting/classes/SampleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace SDKTemplate
+{
+    public partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "Printing C# Sample";
+
+        List<Scenario> scenarios = new List<Scenario>
+        {
+
+        };
+    }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+    }
+}
