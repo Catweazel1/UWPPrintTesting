f3a57da02356aa0dcdeaaef155c0a551deb67d2e
Lonnie Linde <lonnie_linde@hotmail.com>
2019-02-13 08:29:27 +0100

    Adding PageToPrint

--- UWPPrintTesting/classes/PrintHelper.cs
+++ UWPPrintTesting/classes/PrintHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SDKTemplate;
 using Windows.Graphics.Printing;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
--- UWPPrintTesting/classes/SampleConfiguration.cs
+++ UWPPrintTesting/classes/SampleConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Windows.UI.Xaml.Controls;
+using PrintSample;
 
 namespace SDKTemplate
 {
